078b9710990935d624c43747a8831c14ba05d508
VitorBrust1 <dead@thinkpad.localdomain>
2021-08-04 16:57:07 -0300

    Corrigindo mais alguns erros

--- mandelbrot.asm
+++ mandelbrot.asm
@@ -15,6 +15,10 @@ offset: var #1
 n_iterations: var #1
 mf: var #1
 
+; Salvar como variaveis globais os valores de x de y para usar nas iteracoes
+posx: var #1
+posy: var #1
+
 main:
 
     ; Setando o offset que sera usado para fazer as operacoes
@@ -47,6 +51,7 @@ mandelbrot_main:
 
     ; Dois loops enlacados que ciclam por todos os "pixels" da tela
     y_loop:
+        store posy, r5
         loadn r7, #30
         cmp r5, r7
         jeq y_loop_end
@@ -56,6 +61,7 @@ mandelbrot_main:
         loadi r6, r2
         mult r5, r6, r7 
     x_loop:
+        store posx, r4
         loadn r7, #40
         cmp r4, r7
         jeq x_loop_end
@@ -65,7 +71,7 @@ mandelbrot_main:
         loadi r6, r1
         mult r5, r6, r7 
 
-        ; mandelbrot_flag == 0
+        ; mandelbrot_flag == 1
         loadn r3, #1
         store mf, r3
 
@@ -90,6 +96,7 @@ mandelbrot_main:
         loadn r7, #250
         store n_iterations, r7
 
+    ; Loop principal que faz as iteracoes
     iterations_loop:
         ;r2 = (r * r) / offset
         mult r2, r0, r0
@@ -102,11 +109,11 @@ mandelbrot_main:
         div r3, r3, r7
 
         ; Se r_squared + i_squared) > (4 * offset), mf = 0, jmp escape
-        push r6     ; para usar o registrador r6 sem perder a referencia ao contador
+        push r6         ; para usar o registrador r6 sem perder a referencia ao contador
         add r7, r2, r3
         loadn r6, #4xoffset
         cmp r7, r6
-        jgr escape
+        jgr setting_mf
 
         ; im_part = (2 * real_part * im_part)/offset + im0
         mult r7, r0, r1
@@ -127,6 +134,14 @@ mandelbrot_main:
         
     escape:
         ; TODO: colocar aqui o plot dos dos pontos de acordo com o charmap
+        ; usar o posx e posy
+        jmp ending_mandelbrot_iterations
+
+    ; Mandelbrot flag == 0
+    setting_mf:
+        loadn r7, #0
+        store mf, r7
+        jmp escape
         
     ending_mandelbrot_iterations:
         pop r3
@@ -134,13 +149,14 @@ mandelbrot_main:
         pop r1
         pop r0
         
-        inc r1      ; x++
+        inc r1          ; x++
+        loadn r6, #0    ; k == 0, para futuras iteracoes
         jmp x_loop
 
     x_loop_end:
         ; Seta o valor para 0, no caso de ser usado em outro loop ou nao
         loadn r1, #0 
-        inc r2      ; y++
+        inc r2          ; y++
         jmp y_loop
 
     y_loop_end:
